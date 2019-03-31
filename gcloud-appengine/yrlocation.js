c27f69cef2d7d903f81865922f17f04d1b1f5738
ProgrammerChris <chris.tvedt@gmail.com>
2019-04-01 01:06:16 +0200

    new xml-to-json formatter, now for Node not Python...

---
+++ gcloud-appengine/yrlocation.js
@@ -0,0 +1,22 @@
+const https = require('https');
+const xmlToJSON = require('xml-js');
+
+module.exports = {
+    xmlToJson: function(lat, lon)    {
+        // Supplu lat and lon to get weather in JSON format for that location from the YR-API.
+        https.get('https://api.met.no/weatherapi/locationforecast/1.9/?lat=' + lat + '&lon=' + lon, (resp) => {
+            let data = '';
+            // A chunk of data has been recieved.
+            resp.on('data', (chunk) => {
+                data += chunk;
+            });
+            // The whole response has been received. Print out the result.
+            resp.on('end', () => {
+
+                console.log(xmlToJSON.xml2json(data, {compact: true, spaces: 4}));
+            });
+        }).on("error", (err) => {
+            console.log("Error: " + err.message);
+        });
+    }
+}
