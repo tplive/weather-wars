dced0b36eef82a1856ec272b645b1be32de1b056
Thomas Qvidahl <thomas.qvidahl@telenor.com>
2019-04-04 09:26:22 +0200

    Rewrite geocodeThis function to use Maps rest api and return a promise.

--- gcloud-appengine/app.js
+++ gcloud-appengine/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const weatherApi = require('./weatherapi');
+const request = require('request-promise');
 
 // Dotenv parses the contents of the .env file from the working folder.
 // Make your own .env file as it shouldn't be commited to git.
@@ -10,21 +11,38 @@ const app = express();
 
 const API_KEY = process.env.GOOGLE_API_KEY;
 
-const googleMapsClient = require('@google/maps').createClient({
-  key: API_KEY
-})
+async function geocodeThis(address) {
 
-function geocodeThis(address) {
-  googleMapsClient.geocode({
-    address: address
-  }, function(err, response) {
-    if (!err) {
-      console.log(response.json.results[0].geometry.location);
-      return response.json.results[0].geometry.location;
-    }else {
-      return err;
+  var url = "https://maps.googleapis.com/maps/api/geocode/json";
+
+  var options = {
+    uri: url,
+    method: "GET",
+    qs: {
+      address: address,
+      key: API_KEY
+    },
+    json: true
+  }
+
+  try {
+    var result = await request(options);
+    console.log(result.results[0].geometry.location); // This returns the location data
+    var lat = result.results[0].geometry.location.lat; // This is set to the location data parts
+    var lng = result.results[0].geometry.location.lng;
+
+    // This should return the data as an object?
+    return {
+      lat: lat,
+      lng: lng
     }
-  })
+  } catch (err) {
+      console.error(err);
+
+  }
+  
+
+
 }
 
 
@@ -32,8 +50,9 @@ app.get('/', function (req, res) {
     const a = weatherApi.getTemperature("Sometown");
     const b = weatherApi.getTemperature("Otherplace");
     const diff = weatherApi.diffTwoTemperatures(a, b);
-    const location = geocodeThis("Mathias Lunds gate, Stjørdal");
-    // Why isn't the lat, lng coordinates begin returned from geocodeThis??
+    
+    const location = geocodeThis("Mathias Lunds gate, Stjørdal"); // Returned as [object Promise] - promising, I guess.. :P
+    
     sendThis = "Temperature at site A was " + a + "\nTemperature at site B was " + b + "\nDifference is " + diff;
     sendThis += "\nAnd the location is: " + location;
     res.send(sendThis);
