508ae2ad1828d5eb23c6d12fbc91ea7e53f40c17
Thomas Qvidahl <tp@live.no>
2019-04-05 23:10:03 +0200

    Promise should have catch for rejection

--- gcloud-appengine/getweatherfromyr.js
+++ gcloud-appengine/getweatherfromyr.js
@@ -14,6 +14,7 @@ const request = require('request');
 module.exports = {
     asJSON: async function(lat, lon)    {
         // Supply lat and lon to get weather in JSON format for that location from the YR-API.
+        // curl https://api.met.no/weatherapi/locationforecast/1.9/?lat=63.42&lon=10.43
         const url = 'https://api.met.no/weatherapi/locationforecast/1.9/?lat=' + lat + '&lon=' + lon;
 
         function getXML(url)    {
@@ -28,7 +29,9 @@ module.exports = {
             });
         };
 
-        return await getXML(url).then(xml => convert.xml2json(xml, {compact: true, spaces: 4}));
+        return await getXML(url)
+        .then(xml => convert.xml2json(xml, {compact: true, spaces: 4}))
+        .catch(error => {console.log(error)});
     }
 }
 
